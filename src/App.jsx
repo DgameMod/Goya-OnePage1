816928b25cb21b03970c1ad262c6ae0f3522b8a4
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-29 13:00:41 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-blue-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Bienvenido a GOYA - La aplicación perfecta</h1>
+    </div>
+  );
+}
+
+export default App;
