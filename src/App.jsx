f5be03004843df22dcbc79c6bf141787572faa87
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-29 14:21:40 -0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
 import './index.css';
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="font-sans">
+      <Hero />
+      {/* Aquí vendrán About, Productos, etc. */}
+    </div>
+  );
+}
+
+export default App;
+
+
 
 function App() {
   return (
