7c50eabfbbcf41c84cb5fdc209327660600687d1
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-29 14:24:31 -0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,3 @@ function App() {
 }
 
 export default App;
-
-
-
-function App() {
-  return (
-    <div className="min-h-screen bg-white text-blue-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Bienvenido a GOYA - La aplicación perfecta</h1>
-    </div>
-  );
-}
-
-export default App;
